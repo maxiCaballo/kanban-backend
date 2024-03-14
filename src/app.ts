ae1051b6a9cd961368c15f14dc9f191b85a43af6
Maximiliano Caballo <caballomaxi@gmail.com>
2024-03-14 16:06:17 -0300

    Rutas de login y register

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import 'module-alias/register';
 import { Server } from '@/presentation/server';
 import { envs } from '@/config';
+import { AppRoutes } from '@/presentation/routes';
 
 (() => {
 	main();
 })();
 
 async function main() {
-	const server = new Server({ port: envs.PORT });
+	const server = new Server({ port: envs.PORT, routes: AppRoutes.routes });
 
 	server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,21 +4,21 @@ import path from 'path';
 interface Options {
 	port: number;
 	public_path?: string;
-	// routes: Router;
+	routes: Router;
 }
 
 export class Server {
 	public readonly app = express();
 	private readonly port: number;
 	private readonly publicPath: string;
-	// private readonly routes: Router;
+	private readonly routes: Router;
 
 	constructor(options: Options) {
-		const { port, public_path = 'public' /*routes*/ } = options;
+		const { port, public_path = 'public', routes } = options;
 
 		this.port = port;
 		this.publicPath = public_path;
-		// this.routes = routes;
+		this.routes = routes;
 	}
 
 	async start() {
@@ -30,7 +30,7 @@ export class Server {
 		this.app.use(express.static(this.publicPath));
 
 		//*Routes
-		// this.app.use(this.routes);
+		this.app.use(this.routes);
 
 		this.app.get('*', (req, res) => {
 			const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
